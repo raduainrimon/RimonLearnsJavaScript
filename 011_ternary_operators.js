3bf3544b333627e5f637bdc6087d7cb7281872d5
raduainrimon <raduainrimon.education@gmail.com>
2024-07-27 20:16:25 +0600

    ternary operator: compact if-else statement and simpler

---
+++ 011_ternary_operators.js
@@ -0,0 +1,11 @@
+// ternary operator: simpler/ compact if-else statement
+// condition> true output:false output
+// age>13? "teenager" : "kid"
+// ? : ternary operators
+
+let age=18
+age>=13 ? console.log("teenager") : console.log("kid")
+
+// alternative
+// let answer = age>=13 ? "teenager" : "kid"
+// console.log(answer)
