8474ebe60f9cba91ee50040111f31bd0cfc4f671
raduainrimon <raduainrimon.education@gmail.com>
2024-07-27 19:52:32 +0600

    if-else statement

---
+++ 009_if_else_statement.js
@@ -0,0 +1,21 @@
+// let mode = "light"
+// if(mode === "dark")
+//     {
+//         color = "black"
+//         // console.log(color)
+//     } else {
+//             color = "white"
+//             // console.log(color)
+//         }
+
+// console.log(color)
+
+// odd/even problem
+let num = 10
+
+if(num%2 === 0)
+{
+    console.log("Even")
+} else {
+    console.log("Odd") 
+}
