0179e8bbbb2926549627597d2776bbceb3a2edae
raduainrimon <raduainrimon.education@gmail.com>
2024-07-27 19:43:30 +0600

    if statement in JS

---
+++ 008_if_statement.js
@@ -0,0 +1,30 @@
+// let a = 25
+let a = 10
+
+if(a >= 18)
+{
+    console.log("you are eligible for voting")
+}
+
+if(a < 18)
+    {
+        console.log("you are not eligible for voting")
+    }
+
+
+// let mode = "dark"
+// // let mode = "light"
+
+// if(mode === "dark")
+// {
+//     color = "black"
+//     // console.log(color)
+// }
+
+// if(mode === "light")
+//     {
+//         color = "white"
+//         // console.log(color)
+//     }
+
+// console.log(color)
