e3ab92ad52bed77a719511f125b756fb0c29fb16
raduainrimon <raduainrimon.education@gmail.com>
2024-07-27 23:13:06 +0600

    some questions practice

---
+++ 014_practice.js
@@ -0,0 +1,34 @@
+// let num = parseInt(prompt("Type a number (1-100): "))
+let num = prompt("Type a number (1-100): ")
+if (num >= 80 && num <= 100) {
+    console.log("A");
+} else if (num >= 70 && num <= 79) {
+    console.log("B");
+} else if (num >= 60 && num <= 69) {
+    console.log("C");
+} else if (num >= 50 && num <= 59) {
+    console.log("D");
+} else {
+    console.log("F");
+}
+
+
+// let num = 79
+// let grade
+// if (num >= 80 && num <= 100) {
+//     // console.log("A");
+//     grade = "A"
+// } else if (num >= 70 && num <= 79) {
+//     // console.log("B");
+//     grade = "B"
+// } else if (num >= 60 && num <= 69) {
+//     // console.log("C");
+//     grade = "C"
+// } else if (num >= 50 && num <= 59) {
+//     // console.log("D");
+//     grade = "D"
+// } else {
+//     grade = "F"
+// }
+
+// console.log("Grade is:", grade)
