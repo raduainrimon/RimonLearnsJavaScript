b86da6ebffc48cdc1cb45d9d94c086ce68f0adb1
raduainrimon <raduainrimon.education@gmail.com>
2024-07-28 00:45:56 +0600

    for loop cmpletion

---
+++ 015_for_loop.js
@@ -0,0 +1,33 @@
+// Loops are used to execute a piece of code again and again
+
+// Q1: log "Bangladesh" 50 times
+for (let i=1; i<=50; i++)
+{
+    console.log("Bangladesh")
+}
+
+// // Q2: log summation of 1 to n numbers
+// let sum = 0
+// // let i=1
+// // let n=4
+// // for (i=1; i<=n; i++)
+// for (let i=1; i<=4; i++) {
+//     sum+=i
+//     // console.log("",sum)
+// }
+// console.log("Summation is",sum)
+// // console.log(i)
+
+// // Q3: print 1 t0 5
+// // for (var i=1; i<=5; i++){
+// for (let i=1; i<=5; i++){
+//     console.log("i =",i)
+// }
+// // console.log(i) 
+// /*
+// i won't be displayed because i is declared using let(block scope variable) inside for loop
+// if we want to use inside for loop then can use var (global scope variable)
+// */
+
+
+
