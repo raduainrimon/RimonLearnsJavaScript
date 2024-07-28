93a5b488868f787341b4e8686204b720dacb6fbb
raduainrimon <raduainrimon.education@gmail.com>
2024-07-28 12:57:54 +0600

    for-in loops in JS

---
+++ 020_for_in_loop.js
@@ -0,0 +1,16 @@
+// for-in loop (used for displaying object's keys)
+// creating an object named learners 
+let learner = {
+    fullName: "Jubayer Emon",
+    age: 22,
+    cgpa: 3.06,
+    isPass: true,
+};
+// console.log(learner)
+
+// to just print the keys
+for(let i in learner) {
+    console.log("key:",i)
+    // console.log("key:",i, "value:", learner[i]) 
+    // to get the values as well
+}
