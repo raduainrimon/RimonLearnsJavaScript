0ce2fb35b270c796f563be0987c3b5b2ccbbf567
raduainrimon <raduainrimon.education@gmail.com>
2024-07-28 12:24:37 +0600

    019_do_while_loop in JS

---
+++ 018_do_while_loop.js
@@ -0,0 +1,13 @@
+// alternative of for & while loop with slight changes
+// it doesn't matter condition is right or wrong
+// execute at least one time
+// log/print 1-5
+let i=1;
+do{
+    if(i === 1) {
+        console.log("i =",i)
+    } else {
+        console.log("upadated i =",i)
+    }
+    i++
+    } while(i<=5);
