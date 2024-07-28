3473be4659359fac78210c526186504c409ad7e8
raduainrimon <raduainrimon.education@gmail.com>
2024-07-28 23:59:32 +0600

    strings

---
+++ 23_strings.js
@@ -0,0 +1,10 @@
+// Strings in JS
+// Strings is a sequence of characters used to represent text.
+
+// creating strings
+let str = "Raduain Rimon!" // let str2 = 'Hello, Rimon!'
+console.log(str) // printing string
+console.log(str.length) // to get string length
+console.log(str[1])
+
+// str[0], str[1], str[2] // string indices
