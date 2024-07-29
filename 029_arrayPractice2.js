78c9df403c64aba71eed88e4fb3928138e804ce1
raduainrimon <raduainrimon.education@gmail.com>
2024-07-30 00:00:17 +0600

    ARRAY QUESTION 2 MODIFICATION

--- 029_arrayPractice2.js
+++ 029_arrayPractice2.js
@@ -1,6 +1,6 @@
 /* 
             Array Q2
-    ---------------------------
+     -----------------------
 For a given array with prices of 5 items: [250,645,300,900,50]
 All items have an offer of 10%OFF on them.
 Change the array to store final price after applying offer.
