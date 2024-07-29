bf562d3f2fc2a7c142abbaf2acc779b0e12d3ee8
raduainrimon <raduainrimon.education@gmail.com>
2024-07-29 23:51:45 +0600

    array practice Q1

--- 028_arrayPractice.js
+++ 028_arrayPractice.js
@@ -1,4 +1,27 @@
 /* 
-Q: For a given array with marks of students -> [85,97,44,37,76,60]
+Q: For a given array with marks of students -> [8,9,4,3,7,6]
 Find the average marks of the entire class.
 */
+// Rimon's style
+// let arrMarks = [8,9,4,3,7,6]
+// console.log(arrMarks)
+// let sum=0
+// for (let i=0; i<arrMarks.length; i++) {
+//     sum += arrMarks[i]
+// }
+// // console.log(sum)
+// let avg = sum/arrMarks.length
+// console.log(avg)
+
+// Apna College's style
+let arrMarks = [8,9,4,3,7,6]
+let sum = 0
+for (let val of arrMarks) {
+    // console.log(val)
+    sum += val
+}
+console.log(sum)
+let avg = sum/arrMarks.length
+console.log(`average marks of the class = ${avg}`)
+
+
