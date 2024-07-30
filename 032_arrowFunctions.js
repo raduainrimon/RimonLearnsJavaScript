aa3f0ec2321ac19b9c4f35df893f9f269bd36dde
raduainrimon <raduainrimon.education@gmail.com>
2024-07-30 17:21:50 +0600

    arrow functions in JS

---
+++ 032_arrowFunctions.js
@@ -0,0 +1,13 @@
+// compact way of writing a function; function can be store in a variable
+// modern JavaScript
+
+// sum function
+function  sum(a,b) {
+    return a+b
+}
+
+let arrowSum =(a,b) => {
+    console.log(a+b)
+}
+console.log(arrowSum)
+arrowSum(4,5)
