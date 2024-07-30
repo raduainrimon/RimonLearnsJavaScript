42fb1a1cbe9fe01f9a50167abcaedac0a29c1245
raduainrimon <raduainrimon.education@gmail.com>
2024-07-30 17:57:59 +0600

    some modifications in practices

--- 033_functions_practice.js
+++ 033_functions_practice.js
@@ -3,6 +3,29 @@ Create a function using the "function" keyword that
 takes a String as an argument & returns 
 the number of vowels in the string
 */
+// using switch case
+function countVowels(msg) {
+    let vCount = 0
+    for (let i = 0; i < msg.length; i++) {
+        switch (msg[i]) {
+            case 'a': case 'A':
+            case 'e': case 'E':
+            case 'i': case 'I':
+            case 'o': case 'O':
+            case 'u': case 'U':
+                vCount++
+                break
+            default:
+                break; // Do nothing for non-vowel characters
+        }
+    }
+    return vCount
+}
+
+console.log(countVowels("rimOn rAduaIn"))
+
+
+/*
 // using if conditional
 function countVowels(msg) {
     let vCount = 0
@@ -18,31 +41,4 @@ function countVowels(msg) {
     return vCount
 }
 console.log(countVowels("RImon"))
-
-
-/*
-// using switch case
-function countVowels(msg) {
-    let vCount = 0
-    for (let i = 0; i < msg.length; i++) {
-        switch (msg[i]) {
-            case 'a':
-            case 'A':
-            case 'e':
-            case 'E':
-            case 'i':
-            case 'I':
-            case 'o':
-            case 'O':
-            case 'u':
-            case 'U':
-                vCount++
-                break
-            default:
-                break // Do nothing for non-vowel characters
-        }
-    }
-    return vCount
-}
-console.log(countVowels("Rimon"))
 */
