a110b3b4d2dc2c6a5d0b62a865932bbbfc57699a
raduainrimon <raduainrimon.education@gmail.com>
2024-07-30 13:02:43 +0600

    functions basics

---
+++ 031_functions.js
@@ -0,0 +1,37 @@
+/*
+Function: a block of code that performs a specific task, 
+can be invoked(call) whenever needed
+// some built-in functions
+console.log("Rimon")
+"abc".toUpperCase()
+[1,2,3].push(4)
+// log,toUpperCase,push all are functions, always followed by a parenthesis
+// these are invoked for specific tasks. ex. log() for printing
+*/
+
+/*
+// function creation
+// when it is not dependant on input
+function product() { // function=keyWord, product=functionName
+    console.log("Raduain")
+    console.log("Rimon")
+}
+// function calling/invoking
+product() // product=functionName
+*/
+
+/*
+// for a specific message (dependant on input)
+function product(msg) {  // msg=parametre 
+    // parametre->input
+    console.log(msg)
+}
+product("Hello") // this parametre is called argument(value pass)
+*/
+/*
+// practice: adding two numbers using function
+function sum(n1,n2){
+    console.log(n1+n2)
+}
+sum(4,5)
+*/
