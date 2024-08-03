a84523bae6c54887b0d5bdfe33cd4ed0196bf402
raduainrimon <raduainrimon.education@gmail.com>
2024-08-03 15:13:59 +0600

    dom practice

--- 06_DOM_Document_Object_Model_HTML_CSS_JavaScript/dom_logic.js
+++ 06_DOM_Document_Object_Model_HTML_CSS_JavaScript/dom_logic.js
@@ -1,1 +1,12 @@
-alert("RIMON");
+// // accessing element by name
+// let heading = document.getElementById("dom");
+// console.dir(heading);
+
+// let headings = document.getElementsByClassName("dom_learner");
+// console.dir(headings);
+
+// let button = document.getElementById("btn1");
+// console.log(btn1);
+
+let para = document.getElementsByTagName("p");
+console.log(para);
