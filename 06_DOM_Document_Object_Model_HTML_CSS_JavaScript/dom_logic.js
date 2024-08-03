6e7f0198c68e79558522be230be91d6faa426f2f
raduainrimon <raduainrimon.education@gmail.com>
2024-08-03 19:37:19 +0600

    dom practice

--- 06_DOM_Document_Object_Model_HTML_CSS_JavaScript/dom_logic.js
+++ 06_DOM_Document_Object_Model_HTML_CSS_JavaScript/dom_logic.js
@@ -8,5 +8,22 @@
 // let button = document.getElementById("btn1");
 // console.log(btn1);
 
-let para = document.getElementsByTagName("p");
-console.log(para);
+// let para = document.getElementsByTagName("p");
+// console.log(para);
+
+// Query Selector
+// let firstEl = document.querySelector("p"); // will return 1st element
+// console.dir(firstEl);
+// let allEl = document.querySelectorAll("p"); // Will return all matching elements
+// console.dir(allEl);
+
+// class based
+// let firstEl = document.querySelector(".dom_learner"); // will return 1st element
+// console.dir(firstEl);
+// let allEl = document.querySelectorAll(".dom_learner"); // Will return all matching elements
+// console.dir(allEl);
+
+// id based
+let firstEl = document.querySelector("#btn1"); // Will return all matching elements
+console.dir(firstEl);
+console.dir(firstEl.tagName);
