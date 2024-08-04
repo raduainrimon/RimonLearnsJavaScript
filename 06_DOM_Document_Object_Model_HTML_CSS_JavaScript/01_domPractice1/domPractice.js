5d1608ce712e3042098bbec367d14356f1892269
raduainrimon <raduainrimon.education@gmail.com>
2024-08-04 17:10:12 +0600

    dom practice

--- 06_DOM_Document_Object_Model_HTML_CSS_JavaScript/01_domPractice1/domPractice.js
+++ 06_DOM_Document_Object_Model_HTML_CSS_JavaScript/01_domPractice1/domPractice.js
@@ -1,5 +1,19 @@
-let h2 = document.querySelector("h2");
-console.dir(h2.innerText);
+// let h2 = document.querySelector("h2");
+// console.dir(h2.innerText);
 
-h2.innerText = h2.innerText + " from Apna College"; // concatenate
-console.dir(h2.innerText);
+// h2.innerText = h2.innerText + " from Apna College"; // concatenate
+// console.dir(h2.innerText);
+
+let divs = document.querySelectorAll(".box");
+console.log(divs);
+console.log(divs[1]);
+
+// console.log((divs[0].innerText = "Hello Div 1"));
+// console.log((divs[1].innerText = "Hello Div 2"));
+// console.log((divs[2].innerText = "Hello Div 3"));
+// alternative
+let idx = 1;
+for (div of divs) {
+  div.innerText = `Hello Div ${idx}`;
+  idx++;
+}
