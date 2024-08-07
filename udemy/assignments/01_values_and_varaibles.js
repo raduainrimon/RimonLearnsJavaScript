c4ebc673d55d4ab9fa194d4fd43a39d211f9d414
raduainrimon <raduainrimon.education@gmail.com>
2024-08-07 23:49:16 +0600

    udemy assignments solve

---
+++ udemy/assignments/01_values_and_varaibles.js
@@ -0,0 +1,14 @@
+/*LECTURE: Values and Variables 
+1. Declare variables called 'country', 'continent' and 'population' and 
+assign their values according to your own country (population in millions) 
+2. Log their values to the console */
+
+//1
+let country = "Bangladesh";
+let continent = "South-Asia";
+let population = "169";
+
+//2
+console.log(country);
+console.log(continent);
+console.log(population);
